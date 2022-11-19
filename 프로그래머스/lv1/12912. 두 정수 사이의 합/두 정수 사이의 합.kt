902c9feab1949a6a46d4280a27beab4775dba657
sonms <71806591+sonms@users.noreply.github.com>
2022-11-19 23:18:15 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.08 ms, Memory: 63.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun solution(a: Int, b: Int): Long {
+        var answer: Long = 0
+        
+        if (a < b) {
+            for (i in a..b) {
+                answer += i
+            }
+        } else if (a == b) {
+            answer += a
+        } else {
+            for (i in b..a) {
+                answer += i
+            }
+        }
+        
+        return answer
+    }
+}
