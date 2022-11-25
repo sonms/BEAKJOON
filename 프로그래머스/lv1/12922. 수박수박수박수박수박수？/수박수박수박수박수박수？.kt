4c6376ae80e2695f080d7c198889d5bcc6744eb3
sonms <71806591+sonms@users.noreply.github.com>
2022-11-25 23:03:09 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 5.25 ms, Memory: 60.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun solution(n: Int): String {
+        var answer = ""
+        
+        for (i in 0 until n) {
+            if (i % 2 == 0) {
+                answer += "수"
+            } else {
+                answer += "박"
+            }
+        }
+        return answer
+    }
+}
