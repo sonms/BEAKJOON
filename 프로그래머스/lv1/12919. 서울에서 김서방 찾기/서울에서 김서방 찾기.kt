315cd349b6832e80fff58ef7e6e749a87cf2e0d5
sonms <71806591+sonms@users.noreply.github.com>
2023-01-12 21:55:45 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 23.02 ms, Memory: 64.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.kt
@@ -0,0 +1,8 @@
+class Solution {
+    fun solution(seoul: Array<String>): String {
+        val kim = seoul.indexOf("Kim")
+        var answer = "김서방은 ${kim}에 있다"
+        
+        return answer
+    }
+}
