4f9c513acf805de480812b36ce63c5935155e003
sonms <71806591+sonms@users.noreply.github.com>
2023-01-14 16:23:59 +0900

    [level 1] Title: 음양 더하기, Time: 0.06 ms, Memory: 62.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun solution(absolutes: IntArray, signs: BooleanArray): Int {
+        var answer: Int = 0
+        
+        for (i in absolutes.indices) {
+            answer += if (signs[i]) {
+                absolutes[i]
+            } else {
+                -absolutes[i]
+            }
+        }
+        
+        return answer
+    }
+}
