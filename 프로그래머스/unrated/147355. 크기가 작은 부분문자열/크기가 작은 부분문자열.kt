e8e1776f8cc1794d70b3b833e97f2b85e12a9681
sonms <71806591+sonms@users.noreply.github.com>
2023-02-08 20:12:29 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 10.44 ms, Memory: 60.6 MB -BaekjoonHub

--- 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.kt
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.kt
@@ -4,18 +4,17 @@ class Solution {
         val tempArr = arrayListOf<String>()
         val pi = p.toLong()
         for (i in t.indices) {
-            if (i + p.length > t.length) {
-                
-            } else {
+            if (i + p.length <= t.length) {
                 val ts = t.slice(i until i + p.length)
                 if (ts.toLong() <= pi) {
                     answerArr.add(ts.toLong())
                 }
+                //tempArr.add(ts)
             }
         }
-        /*val an = tempArr.filter {it.toInt() <= p.toInt()}
+        /*val an = tempArr.filter {it.toLong() <= p.toLong()}
         for (j in an.indices) {
-            answerArr.add(an[j].toInt())
+            answerArr.add(an[j].toLong())
         }*/
     
         return answerArr.size
