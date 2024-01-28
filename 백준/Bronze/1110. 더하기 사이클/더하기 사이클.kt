3c07f7cbd31f46fa7b94cd92d6b048961f7c40ef
sonms <71806591+sonms@users.noreply.github.com>
2024-01-28 19:40:14 +0900

    [Bronze I] Title: 더하기 사이클, Time: 92 ms, Memory: 12364 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.kt
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.kt
@@ -1,4 +1,27 @@
 fun main() {
+    val input = readln().toInt()
+    var cnt = 0
+    var newNum = input
+    var sum = 0
+
+    while (true) { //26
+        val a = newNum / 10 // 2
+        var b = newNum % 10 // 6
+        sum = a+b
+        b *= 10
+
+        newNum = b+sum%10 // 6 sum%10 = 8%10 = 8 6+8 = 14 바로 다음행
+        cnt++
+
+        if (newNum == input) {
+            break
+        }
+    }
+    print(cnt)
+}
+
+/*
+fun main() {
     val input = readln().toString()
     var cnt = 0
     var newNum = "-1"
@@ -19,4 +42,4 @@ fun main() {
         }
     }
     println(cnt)
-}
+}*/
