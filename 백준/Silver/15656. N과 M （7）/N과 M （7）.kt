53d163617ad4ba9b94f6fb4f84a700b3b099ceb5
sonms <71806591+sonms@users.noreply.github.com>
2024-04-13 15:28:06 +0900

    [Silver III] Title: N과 M (7), Time: 1408 ms, Memory: 364016 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.kt
@@ -0,0 +1,49 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+private val result = ArrayList<String>()
+
+fun generatePermutations(
+    nums: List<Int>, //탐색할 배열
+    tempList: MutableList<Int>, // 임시 리스트
+    used: BooleanArray, // 방문 여부를 나타내는 배열
+    length: Int, //현재 순열의 길이
+    targetLength: Int //원하는 순열의 길이
+) {
+    if (length == targetLength) {
+        result.add(tempList.joinToString(" "))
+        return
+    }
+    for (i in nums.indices) {
+        if (!used[i]) {
+            tempList.add(nums[i])
+            used[i] = false
+            generatePermutations(nums, tempList, used, length + 1, targetLength)
+            tempList.removeAt(tempList.size - 1)
+        }
+    }
+}
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val (n,m) = br.readLine().split(" ").map { it.toInt() }
+    val nums = br.readLine().split(" ").map { it.toInt() }.sorted() // 순열을 생성할 리스트
+
+
+    val tempList = mutableListOf<Int>() // 임시 리스트
+    val used = BooleanArray(nums.size) // 방문 여부를 나타내는 배열
+    /*val targetLength = nums.size*/ // 원하는 순열의 길이
+
+    generatePermutations(nums, tempList, used, 0, m)
+
+    result.forEach {
+        bw.write(it)
+        bw.newLine()
+    }
+    bw.flush()
+    bw.close()
+}
