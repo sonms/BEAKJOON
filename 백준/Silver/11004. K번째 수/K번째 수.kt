cd21dfd29b3029cf49956c15569c3ad2aa93cb7a
sonms <71806591+sonms@users.noreply.github.com>
2024-04-19 20:55:35 +0900

    [Silver V] Title: K번째 수, Time: 4756 ms, Memory: 975900 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.kt
@@ -0,0 +1,14 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val (n, k) = br.readLine().split(" ").map { it.toInt() }
+    val num = br.readLine().split(" ").map { it.toInt() }.sorted()
+    println(num[k-1])
+
+}
