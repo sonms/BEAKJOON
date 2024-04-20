59fc0e5aef3f04213edab95614bd4291fb2e8006
sonms <71806591+sonms@users.noreply.github.com>
2024-04-20 14:52:50 +0900

    [Silver V] Title: 중복 빼고 정렬하기, Time: 392 ms, Memory: 38480 KB -BaekjoonHub

---
+++ 백준/Silver/10867. 중복 빼고 정렬하기/중복 빼고 정렬하기.kt
@@ -0,0 +1,14 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val n = br.readLine().toInt()
+    val data = br.readLine().split(" ").map { it.toInt() }.toSet().sorted()
+
+    println(data.joinToString(" "))
+}
