c759eed4c65c5ea855c9a0a7d5f7d84c9b6fedb2
sonms <71806591+sonms@users.noreply.github.com>
2024-09-15 13:00:21 +0900

    [level 1] Title: 체육복, Time: 33.03 ms, Memory: 64 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.kt
@@ -0,0 +1,37 @@
+class Solution {
+    fun solution(n: Int, lost: IntArray, reserve: IntArray): Int {
+        var answer = 0  
+
+        val receivableStudent = arrayListOf<Int>()
+
+        val commonElements = lost.intersect(reserve.toList().toSet())
+    /*println("교집합 $commonElements")
+    println("차집합 ${reserve.subtract(commonElements).sorted()}")
+    println("lost ${lost.subtract(commonElements).sorted()}")
+*/
+        for (i in reserve.subtract(commonElements).sorted()) {
+            val checkLost = lost.subtract(commonElements).sorted().find {
+                if (!receivableStudent.contains(it)) {
+                    it == i - 1 || it == i + 1
+                } else {
+                    false
+                }
+        }
+        //println(checkLost)
+            checkLost?.let {
+                receivableStudent.add(it)
+            }
+       /* if (!receivableStudent.contains(checkLost)) {
+            checkLost.let {
+                receivableStudent.add(it)
+                println("checkList $checkLost")
+                println("add $it")
+            }
+        }*/
+        //println(checkLost)
+        }
+        
+        answer = n - lost.size + receivableStudent.size + commonElements.size
+        return answer
+    }
+}
