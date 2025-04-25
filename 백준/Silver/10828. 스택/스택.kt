0f3c07df99addb6624adc054908101530f96a4e6
sonms <71806591+sonms@users.noreply.github.com>
2025-04-25 18:46:07 +0900

    [Silver IV] Title: 스택, Time: 212 ms, Memory: 26588 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.kt
+++ 백준/Silver/10828. 스택/스택.kt
@@ -1,48 +1,25 @@
+import java.io.BufferedReader
 import java.io.BufferedWriter
+import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 import java.util.*
 
 fun main() {
     val stack : Stack<Int> = Stack()
+    val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
 
-    val n = readln().toInt()
+    val n = br.readLine().toInt()
 
     repeat(n) {
-        val input = readln().split(" ").map { it.toString() }
+        val input = br.readLine().split(" ").map { it }
 
-        if (input.size > 1) {
-            stack.push(input[1].toInt())
-        } else {
-            if (input[0] == "pop") {
-                if (stack.isEmpty()) {
-                    bw.write("-1")
-                    bw.newLine()
-                } else {
-                    bw.write(stack.pop().toString())
-                    bw.newLine()
-                }
-            } else if (input[0] == "size") {
-                bw.write(stack.size.toString())
-                bw.newLine()
-            } else if (input[0] == "empty") {
-                if (stack.isEmpty()) {
-                    bw.write("1")
-                    bw.newLine()
-                } else {
-                    bw.write("0")
-                    bw.newLine()
-                }
-            } else if (input[0] == "top") {
-                if (stack.isEmpty()) {
-                    bw.write("-1")
-                    bw.newLine()
-                } else {
-                    //println(stack.peek())
-                    bw.write(stack.peek().toString())
-                    bw.newLine()
-                }
-            }
+        when (input[0]) {
+            "push" -> stack.push(input[1].toInt())
+            "pop" -> bw.write("${stack.removeLastOrNull() ?: -1}\n")
+            "size" -> bw.write("${stack.size}\n")
+            "empty" -> bw.write("${if (stack.isEmpty()) 1 else 0}\n")
+            "top" -> bw.write("${stack.lastOrNull() ?: -1}\n")
         }
     }
 
