ab14efa572fcc8691a6b9b587798d847ffe3157b
sonms <71806591+sonms@users.noreply.github.com>
2025-05-04 18:21:02 +0900

    [Silver III] Title: 파도반 수열, Time: 76 ms, Memory: 12212 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.kt
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.kt
@@ -1,26 +1,16 @@
-fun main() {
-    val input = readLine()!!.toInt()
-    val dp = LongArray(101){_ -> 0}
-    val result = arrayListOf<Long>()
-    val sb = StringBuilder()
-    dp[1] = 1
-    dp[2] = 1
-    dp[3] = 1
-    dp[4] = 2
-    dp[5] = 2
-    for (i in 1..input) {
-        val input2 = readLine()!!.toInt()
-        if (input2 < 6) {
-            result.add(dp[input2])
-        } else {
-            for (j in 6..input2) {
-                dp[j] = dp[j-5] + dp[j-1]
-            }
-            result.add(dp[input2])
-        }
+fun main() = with(System.`in`.bufferedReader()) {
+    val t = readLine().toInt()
+    val dp = LongArray(101) { 0L }
+    dp[1] = 1L
+    dp[2] = 1L
+    dp[3] = 1L
+    for (i in 4..100) {
+        dp[i] = dp[i - 2] + dp[i - 3]
     }
-    result.forEach {
-        sb.append(it).append("\n")
+    val sb = StringBuilder()
+    repeat(t) {
+        val n = readLine().toInt()
+        sb.appendLine(dp[n])
     }
     print(sb)
 }
