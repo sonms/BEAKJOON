550471f2f3145269956ffe2ca906cc34ed8a29b2
sonms <71806591+sonms@users.noreply.github.com>
2025-06-03 01:05:58 +0900

    [Silver IV] Title: 수 찾기, Time: 1868 ms, Memory: 84840 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.kt
+++ 백준/Silver/1920. 수 찾기/수 찾기.kt
@@ -1,62 +1,30 @@
-fun binarySearch(arr : List<Int>, find :Int, low : Int, high : Int ):Int {
-   var low = low
-   var high = high
-   var mid = 0
-   while (low <= high) {
-      mid = (low+high) / 2
-      when{
-         arr[mid] == find->return 1
-         arr[mid] > find->high = mid - 1
-         else -> low = mid + 1
-      }
-   }
-   return 0
-}
+val br = System.`in`.bufferedReader()
+
 fun main() {
-   val input = readLine()!!.toInt()
-   var arr = arrayListOf<Int>()
-   val re = arrayListOf<Int>()
-   val a = readLine()!!.split(" ")
-   for (i in 0 until input) {
-      val t = a[i].toInt()
+   val n = br.readLine().toInt()
+   val nArr = br.readLine().split(" ").map { it.toInt() }
 
-      arr.add(t)
-   }
-   val inp = readLine()!!.toInt()
-   val m = readLine()!!.split(" ")
-   for (j in 0 until inp) {
-      val c = m[j].toInt()
+   val m = br.readLine().toInt()
+   val mArr = br.readLine().split(" ").map { it.toInt() }
 
-      re.add(c)
+   val sort = nArr.sorted()
+
+   for (i in mArr) {
+      val exists = binarySearch(sort, i, 0, n - 1)
+      println(if (exists) 1 else 0)
    }
-   val sort = arr.sorted().toMutableList()
-   for (k in 0 until inp) {
-      var check = re[k]
-      var result = binarySearch(sort,check,0,input-1)
-      println(result)
-   }
-   /*for (j in 0 until inp) {
-      val check = m[j].toInt()
-      if (arr.contains(check)) {
-         re.add(1)
-      } else {
-         re.add(0)
-      }
-   }*/
-   /*val sort = arr.sorted()
-   var low = 0
-   var high = arr.maxOrNull()!!
-   var mid = 0
-   var target = 0
-   while (low <= high) {
-      mid = (low + high) / 2
-      when{
-         arr[mid] == target -> mid
-         arr[mid] > target -> high = mid - 1
-         else -> low = mid+1
+}
+
+fun binarySearch(arr: List<Int>, target: Int, low: Int, high: Int): Boolean {
+   var l = low
+   var h = high
+   while (l <= h) {
+      val mid = (l + h) / 2
+      when {
+         arr[mid] == target -> return true
+         arr[mid] > target -> h = mid - 1
+         else -> l = mid + 1
       }
    }
-   re.forEach {
-      println(it)
-   }*/
+   return false
 }
