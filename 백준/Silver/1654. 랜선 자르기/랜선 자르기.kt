0a1a9f3b797e5ac8d66b3a3a2026a9686d9d94c3
sonms <71806591+sonms@users.noreply.github.com>
2025-06-03 01:29:14 +0900

    [Silver II] Title: 랜선 자르기, Time: 204 ms, Memory: 24228 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.kt
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.kt
@@ -1,32 +1,32 @@
+val br = System.`in`.bufferedReader()
+
 fun main() {
-    val input = readLine()!!.split(" ")
-    val k = input[0].toInt()
-    val n = input[1].toInt()
-    var arr = mutableListOf<Long>()
-    var max : Long = 0
-    var min : Long = 0 //탐색길이 최솟값
-    var mid : Long = 0 //범위내의 중간 길이 값
-    for (i in 1..k){
-        val t = readLine()!!.toLong()
-        arr.add(t)
-    }
-    max = arr.maxOrNull()!!
-    max++
-    while (min < max) {
-        mid = (max + min) / 2
-        var cnt : Long = 0
-        for (j in 0 until  arr.size) {
-            cnt += arr[j] / mid
-        }
-        //mid길이로 잘랐을 때 개수가 만들고자 하는 랜선의 개수보다 작다면
-        //자르고자 하는 길이를 줄이기 위해 최대 길이를 줄인다
-        //그 외에는 자르고자 하는 길이를 늘려야 하므로 최소길이를 늘린다.
-        if (cnt < n) {
-            max = mid
-        } else {
-            min = mid + 1
-        }
-    }
-    //upperbound로 얻어진 값에 -1이 최대 길이
-    println(min - 1)
+   val (k,n) = br.readLine().split(" ").map {it.toInt()}
+
+   //전선용
+   val arr = LongArray(k) { br.readLine().toLong() }
+
+   //이분 탐색의 상한 방법 upperbound
+   //상한 = 찾고자 하는 특정 값을 초과하는 첫 위치
+
+   //랜선 최소 길이
+   var low = 1L
+   //랜선의 가장 긴 길이
+   var high = arr.maxOrNull()!!
+
+   while (low <= high) {
+      val mid = (low + high) / 2
+      //현재 mid 길이로 자르면 만들 수 있는 총 랜선 개수
+      val p = arr.sumOf { it / mid }
+
+      //필요 개수보다 적게ㅏㄴ오면
+      if (p < n) {
+         high = mid - 1 //길이를 줄여야함 mid로 줄임
+      } else {
+         //개수가 충분히 많이 나온다면
+         low = mid + 1 //길이를 늘려 최대길이를 찾을 수 있도록 low증가
+      }
+   }
+   //이분탐색으로 얻어진 값에 -1이 최대 길이
+   println(low - 1)
 }
